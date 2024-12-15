9ccfd6499e0bd1b56876a9e0f00936d068f99720
rares <rares@vbox.rares.com>
2024-12-15 15:50:36 +0200

    computed properties in user inherited form view - used a button w action that triggers a list view - still mem error

--- odoo/addons/estate/models/inherited_users.py
+++ odoo/addons/estate/models/inherited_users.py
@@ -3,34 +3,45 @@ from odoo import models, fields, api
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    # property_ids = fields.One2many(
-    #     'estate.property',  
-    #     'salesPerson_id',   
-    #     string='Properties',
-    #     domain=[('state', 'in', ['new', 'offer_received'])],  # Domain to only list available properties
-    #     # domain=[('state', '=', 'new')],  # Domain to only list available properties
-    # )
+    property_ids = fields.One2many(
+        'estate.property',  
+        'salesPerson_id',   
+        string='Properties',
+        domain=[('state', 'in', ['new', 'offer_received'])],  # Domain to only list available properties
+        # domain=[('state', '=', 'new')],  # Domain to only list available properties
+    )
 
     # property_subset_ids = fields.One2many(
     #     'estate.property',  
     #     compute='_compute_property_subset',
     # )
-    property_subset_ids = fields.One2many(
-        'estate.property',
-        'salesPerson_id',
-        string='Property Subset',
-        compute='_compute_property_subset',
-        store=False,
-        help="Dynamically computes a subset of properties assigned to the salesperson."
-    )
+    # property_subset_ids = fields.One2many(
+    #     'estate.property',
+    #     'salesPerson_id',
+    #     string='Property Subset',
+    #     compute='_compute_property_subset',
+    #     store=False,
+    #     help="Dynamically computes a subset of properties assigned to the salesperson."
+    # )
 
-    # @api.depends('id') @ without this, the method is invoked whenever the field is accessed in the view
-    def _compute_property_subset(self):
-        for user in self:
-            # Load only 30 records, adjust the order as needed
-            properties = self.env['estate.property'].search([
-                ('salesPerson_id', '=', user.id),
-                ('state', 'in', ['new', 'offer_received'])
-            ], limit=30)  # Limit the number of records to 30
-            user.property_subset_ids = properties
-            # user.property_subset_ids = user.property_ids[:30]
+    # @api.depends('id') # without this, the method is invoked whenever the field is accessed in the view
+    # def _compute_property_subset(self):
+    #     for user in self:
+    #         # Load only 30 records, adjust the order as needed
+    #         properties = self.env['estate.property'].search([
+    #             ('salesPerson_id', '=', user.id),
+    #             ('state', 'in', ['new', 'offer_received'])
+    #         ], limit=30)  # Limit the number of records to 30
+    #         user.property_subset_ids = properties
+    #         # user.property_subset_ids = user.property_ids[:30]
+
+    def action_open_user_properties(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Properties',
+            'res_model': 'estate.property',
+            'view_mode': 'list,form',
+            'domain': [('id', 'in', self.property_ids.ids)],
+            'context': {'default_user_id': self.id},
+        }
